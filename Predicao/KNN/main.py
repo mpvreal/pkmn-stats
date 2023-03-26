507213dcfdbeba19eb221f982d0b6abf5cb7a925
felipe based mito <42080804+felipeyousoro@users.noreply.github.com>
2023-03-25 22:42:13 -0300

    notebook KNN

--- Predicao/KNN/main.py
+++ Predicao/KNN/main.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
 
 CSV_FULL_PATH = '../CSVs/pokemon.csv'
 
@@ -10,22 +9,22 @@ if __name__ == '__main__':
     data_pandas = pd.read_csv(CSV_FULL_PATH)
     df = pd.DataFrame(data_pandas)
 
-    x = df.drop(['is_legendary'], axis=1)
+    x = df.drop(['attack', 'defense', 'sp_attack', 'sp_defense', 'is_legendary', 'name'], axis=1)
     y = df['is_legendary']
 
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.6, random_state=42)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=1)
 
     training_accuracy = []
     test_accuracy = []
 
-    neighbors_settings = range(1, 20)
+    neighbors_settings = range(1, 16)
 
     for n_neighbors in neighbors_settings:
-        clf = KNeighborsClassifier(n_neighbors=n_neighbors)
-        clf.fit(x_train, y_train)
+        model = KNeighborsClassifier(n_neighbors=n_neighbors)
+        model.fit(x_train, y_train)
 
-        training_accuracy.append(clf.score(x_train, y_train))
-        test_accuracy.append(clf.score(x_test, y_test))
+        training_accuracy.append(model.score(x_train, y_train))
+        test_accuracy.append(model.score(x_test, y_test))
 
     fig = plt.figure(figsize=(10, 5))
     plt.plot(neighbors_settings, training_accuracy, label='Training Accuracy')
